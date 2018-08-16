837073f4717b367b269856897aa3d76b1ccc2121
Betrezen88 <michal.s.wyszynski@gmail.com>
2018-08-16 20:38:56 +0200

    Added QLabel style for title label in Section.

--- NPCEditor/Section/Section.h
+++ NPCEditor/Section/Section.h
@@ -2,6 +2,8 @@
 #define SECTION_H
 
 #include <QWidget>
+#include <QVBoxLayout>
+#include <QJsonObject>
 
 class Section : public QWidget
 {
@@ -9,12 +11,18 @@ class Section : public QWidget
 public:
     explicit Section(QWidget *parent = nullptr): QWidget(parent) {}
 
-signals:
-
-public slots:
+protected:
+    virtual QVBoxLayout *createLayout(const QJsonArray& json) = 0;
 
 protected:
-    const QString m_titleStyle{};
+    const QString m_titleStyle{ "QLabel{"
+                                " padding: 5px;"
+                                " background-color: black;"
+                                " color: white;"
+                                " border-radius: 5px;"
+                                " font: 20px;"
+                                " qproperty-alignment: AlignCenter;"
+                                "}" };
 };
 
 #endif // SECTION_H
