4741d9b93dff19ef6c6bc7864504c7cea3ea0f1f
Betrezen88 <michal.s.wyszynski@gmail.com>
2018-08-20 22:49:22 +0200

    Added basic layout of SkillPacksWidget.

--- NPCEditor/Widget/SkillPacksWidget.cpp
+++ NPCEditor/Widget/SkillPacksWidget.cpp
@@ -1,21 +1,42 @@
-#include "SkillPacksWidget.h"
+﻿#include "SkillPacksWidget.h"
 
 #include <QJsonObject>
 #include <QJsonValue>
-#include <QLabel>
-#include <QVBoxLayout>
+#include <QGridLayout>
+#include <QStyleOption>
+#include <QPainter>
 
 SkillPacksWidget::SkillPacksWidget(const QString &name, const QJsonArray &skills, QWidget *parent)
-    : QWidget(parent)
+    : QWidget(parent),
+      m_pTitle(new QLabel(name, this))
 {
-    QVBoxLayout* all = new QVBoxLayout;
+    setObjectName("A");
+    m_pTitle->setStyleSheet( m_titleStyle );
+    m_pTitle->setFixedHeight( 30 );
+    setStyleSheet( m_widgetStyle );
+    setFixedHeight( 110 );
 
-    all->addWidget( new QLabel(name, this) );
+    QGridLayout* all = new QGridLayout;
 
+    all->addWidget( m_pTitle, 0, 0, 1, 0 );
+
+    int row = 1;
     for ( const QJsonValue &pack: skills ) {
         QLabel *pLabel = new QLabel(pack.toString(), this);
-        all->addWidget( pLabel );
+        QSpinBox *pSpinBox = new QSpinBox(this);
+        m_skills.insert( pack.toString(), pSpinBox );
+        all->addWidget( pLabel, row, 0 );
+        all->addWidget( pSpinBox, row, 1 );
+        ++row;
     }
 
     setLayout( all );
+}
+
+void SkillPacksWidget::paintEvent(QPaintEvent *)
+{
+    QStyleOption opt;
+    opt.init(this);
+    QPainter p(this);
+    style()->drawPrimitive(QStyle::PE_Widget, &opt, &p, this);
 }
--- NPCEditor/Widget/SkillPacksWidget.h
+++ NPCEditor/Widget/SkillPacksWidget.h
@@ -1,18 +1,34 @@
-#ifndef SKILLPACKSWIDGET_H
+﻿#ifndef SKILLPACKSWIDGET_H
 #define SKILLPACKSWIDGET_H
 
 #include <QWidget>
 #include <QJsonArray>
+#include <QHash>
+#include <QLabel>
+#include <QSpinBox>
 
 class SkillPacksWidget : public QWidget
 {
     Q_OBJECT
 public:
-    explicit SkillPacksWidget(const QString &name, const QJsonArray &skills, QWidget *parent = nullptr);
+    explicit SkillPacksWidget(const QString &name,
+                              const QJsonArray &skills,
+                              QWidget *parent = nullptr);
+    void paintEvent(QPaintEvent *);
 
-signals:
-
-public slots:
+private:
+    QLabel *m_pTitle{nullptr};
+    QHash<QString, QSpinBox*> m_skills;
+    const QString m_widgetStyle{ "QWidget#A{"
+                                 " border: 1px solid black;"
+                                 " border-radius: 10px;"
+                                 "}" };
+    const QString m_titleStyle{ "QLabel{"
+                                " font: 12px;"
+                                " font-weight: bold;"
+                                " padding-left: 5px;"
+                                " padding-bottom: 5px;"
+                                "}" };
 };
 
 #endif // SKILLPACKSWIDGET_H
