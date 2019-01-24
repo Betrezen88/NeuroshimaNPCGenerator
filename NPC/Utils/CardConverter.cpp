6705980b38f1e389d8bfee0c983fca1209435264
Betrezen88 <wyszyn2000@gmail.com>
2019-01-24 15:29:12 +0100

    Added method toJson() to CardConverter.

--- NPC/Utils/CardConverter.cpp
+++ NPC/Utils/CardConverter.cpp
@@ -1,6 +1,17 @@
 #include "CardConverter.h"
 
+#include <QJsonValue>
+
 CardConverter::CardConverter()
 {
 
+}
+
+const QJsonObject CardConverter::toJson(const NPCCardTab *card) const
+{
+    QJsonObject cardJson;
+
+    cardJson.insert( "personal", QJsonValue("") );
+
+    return cardJson;
 }
--- NPC/Utils/CardConverter.h
+++ NPC/Utils/CardConverter.h
@@ -1,11 +1,16 @@
 #ifndef CARDCONVERTER_H
 #define CARDCONVERTER_H
 
+#include <QJsonObject>
+
+#include "NPC/NPCCardTab.h"
 
 class CardConverter
 {
 public:
     CardConverter();
+
+    const QJsonObject toJson(const NPCCardTab *card) const;
 };
 
 #endif // CARDCONVERTER_H
