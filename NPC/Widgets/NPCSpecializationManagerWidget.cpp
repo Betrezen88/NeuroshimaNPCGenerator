d2018dca9cf8677873a311ca7796723979a75254
Betrezen88 <wyszyn2000@gmail.com>
2019-02-12 12:59:27 +0100

    Added descriptions to specializations.

---
+++ NPC/Widgets/NPCSpecializationManagerWidget.cpp
@@ -0,0 +1,6 @@
+#include "NPCSpecializationManagerWidget.h"
+
+NPCSpecializationManagerWidget::NPCSpecializationManagerWidget(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ NPC/Widgets/NPCSpecializationManagerWidget.h
@@ -0,0 +1,17 @@
+#ifndef NPCSPECIALIZATIONMANAGERWIDGET_H
+#define NPCSPECIALIZATIONMANAGERWIDGET_H
+
+#include <QWidget>
+
+class NPCSpecializationManagerWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit NPCSpecializationManagerWidget(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // NPCSPECIALIZATIONMANAGERWIDGET_H
