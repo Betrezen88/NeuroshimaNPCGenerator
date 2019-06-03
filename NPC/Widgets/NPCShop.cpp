efb3eb2289959ad14a177a3f7171fc8b799d62ee
Betrezen88 <wyszyn2000@gmail.com>
2019-06-03 15:51:56 +0200

    Added functionality to select category and subcategory in shop.

--- NPC/Widgets/NPCShop.cpp
+++ NPC/Widgets/NPCShop.cpp
@@ -3,6 +3,7 @@
 #include "../Utils/DataLoader.h"
 #include "NPCShopItem.h"
 
+#include <QComboBox>
 #include <QGridLayout>
 #include <QJsonArray>
 #include <QJsonObject>
@@ -10,20 +11,50 @@
 #include <QLabel>
 #include <QListWidget>
 
+#include <QDebug>
+
 NPCShop::NPCShop(QWidget *parent)
     : QWidget(parent),
+      m_pCategory(new QComboBox(this)),
+      m_pSubcategory(new QComboBox(this)),
       m_pInventory(new QListWidget(this)),
       m_pShop(new QListWidget(this))
 {
+    connect( m_pCategory, &QComboBox::currentTextChanged,
+             this, &NPCShop::setSubcategory );
+    connect( m_pSubcategory, &QComboBox::currentTextChanged,
+             this, &NPCShop::showItems );
+
     QGridLayout *pLayout = new QGridLayout;
+    pLayout->setSpacing( 1 );
     setLayout( pLayout );
 
     addItemsToShop();
 
-    pLayout->addWidget( new QLabel("Ekwipunek"), 0, 0 );
-    pLayout->addWidget( m_pInventory, 1, 0 );
-    pLayout->addWidget( new QLabel("Sklep"), 0, 1 );
-    pLayout->addWidget( m_pShop, 1, 1 );
+    pLayout->addWidget( new QLabel("Ekwipunek"), 0, 0, 1, 2 );
+    pLayout->addWidget( m_pInventory, 1, 0, 7, 2 );
+    pLayout->addWidget( new QLabel("Sklep"), 0, 2, 1, 2 );
+    pLayout->addWidget( new QLabel("Kategoria"), 1, 2, 1, 1 );
+    pLayout->addWidget( m_pCategory, 1, 3, 1, 1 );
+    pLayout->addWidget( new QLabel("Podkategoria"), 2, 2, 1, 1 );
+    pLayout->addWidget( m_pSubcategory, 2, 3, 1, 1 );
+    pLayout->addWidget( m_pShop, 3, 2, 5, 2 );
+}
+
+void NPCShop::setSubcategory(const QString &categoryName)
+{
+    m_pSubcategory->clear();
+    m_pSubcategory->addItems( m_items.value(categoryName).keys() );
+}
+
+void NPCShop::showItems(const QString &subcategoryName)
+{
+    if ( !subcategoryName.isEmpty() ) {
+        hideItems();
+
+        for ( QListWidgetItem *pItem: m_items.value(m_pCategory->currentText()).value(subcategoryName) )
+            pItem->setHidden( false );
+    }
 }
 
 void NPCShop::addItemsToShop()
@@ -32,15 +63,31 @@ void NPCShop::addItemsToShop()
 
     for ( const QJsonValue category: categories ) {
         const QJsonObject tCategory = category.toObject();
+        QHash<QString, QVector<QListWidgetItem*>> subcategories;
+
         for ( const QJsonValue subcategory: tCategory.value("subcategories").toArray() ) {
             const QJsonObject tSubcategory = subcategory.toObject();
+
+            QVector<QListWidgetItem*> items;
             for ( const QJsonValue item: tSubcategory.value("items").toArray() ) {
-                QListWidgetItem *pItem = new QListWidgetItem( m_pShop );
+                QListWidgetItem *pItem = new QListWidgetItem(m_pShop);
                 NPCShopItem *pShopItem = new NPCShopItem(item.toObject(), this);
-                pItem->setSizeHint( pShopItem->sizeHint() );
                 m_pShop->addItem( pItem );
                 m_pShop->setItemWidget( pItem, pShopItem );
+                pItem->setSizeHint( pShopItem->sizeHint() );
+                items.push_back( pItem );
             }
+            subcategories.insert( tSubcategory.value("name").toString(), items );
         }
+        m_items.insert( tCategory.value("category").toString(), subcategories );
+    }
+
+    m_pCategory->addItems( m_items.keys() );
+}
+
+void NPCShop::hideItems()
+{
+    for ( int i{0}; i<m_pShop->count(); ++i ) {
+        m_pShop->item(i)->setHidden( true );
     }
 }
--- NPC/Widgets/NPCShop.h
+++ NPC/Widgets/NPCShop.h
@@ -2,8 +2,17 @@
 #define NPCSHOP_H
 
 #include <QWidget>
+#include <QHash>
 
+class QComboBox;
 class QListWidget;
+class QListWidgetItem;
+
+class NPCShopItem;
+
+struct Category {
+
+};
 
 class NPCShop : public QWidget
 {
@@ -15,12 +24,22 @@ signals:
 
 public slots:
 
+private slots:
+    void setSubcategory(const QString &categoryName);
+    void showItems(const QString &subcategoryName);
+
 private:
     void addItemsToShop();
 
+    void hideItems();
+
 private:
+    QComboBox *m_pCategory{nullptr};
+    QComboBox *m_pSubcategory{nullptr};
     QListWidget *m_pInventory{nullptr};
     QListWidget *m_pShop{nullptr};
+
+    QHash<QString, QHash<QString, QVector<QListWidgetItem*>>> m_items;
 };
 
 #endif // NPCSHOP_H
