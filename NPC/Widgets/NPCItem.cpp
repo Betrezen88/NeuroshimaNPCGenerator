eff9b3e9f1feb27df0b691a3503cac1e7201db58
Betrezen88 <wyszyn2000@gmail.com>
2019-07-10 12:21:49 +0200

    Reworked NPCItem.

--- NPC/Widgets/NPCItem.cpp
+++ NPC/Widgets/NPCItem.cpp
@@ -1,23 +1,20 @@
 #include "NPCItem.h"
 
+#include <QAction>
 #include <QHBoxLayout>
 #include <QLabel>
+#include <QMenu>
 #include <QPushButton>
 
-NPCItem::NPCItem(const QJsonObject &item, QWidget *parent)
+NPCItem::NPCItem(const QJsonObject &item, Type type, QWidget *parent)
     : NPCCustomWidget(parent),
       m_item( item ),
+      m_type( type ),
       m_pName(new QLabel(m_item.value("name").toString(), this)),
       m_pQuantity(new QLabel("1", this)),
-      m_pReturnBtn(new QPushButton("Zwróć", this))
+      m_pButton(new QPushButton("...", this))
 {
-    connect( m_pReturnBtn, &QPushButton::clicked,
-             [this](){
-        emit this->returnItem(m_item.value("name").toString(), m_item.value("price").toInt());
-        m_pQuantity->setText( QString::number(m_pQuantity->text().toInt()-1) );
-        if ( m_pQuantity->text().toInt() == 0 )
-            emit this->destroyItem( this );
-    });
+    createActions();
 
     QHBoxLayout *pLayout = new QHBoxLayout;
     pLayout->setSpacing( 1 );
@@ -25,7 +22,7 @@ NPCItem::NPCItem(const QJsonObject &item, QWidget *parent)
     pLayout->addWidget( m_pName, 0, Qt::AlignLeft );
     pLayout->addSpacerItem( new QSpacerItem(1, 1, QSizePolicy::Maximum, QSizePolicy::MinimumExpanding) );
     pLayout->addWidget( m_pQuantity, 0, Qt::AlignRight );
-    pLayout->addWidget( m_pReturnBtn, 0, Qt::AlignRight );
+    pLayout->addWidget( m_pButton, 0, Qt::AlignRight );
 }
 
 QString NPCItem::name() const
@@ -33,7 +30,87 @@ QString NPCItem::name() const
     return m_item.value("name").toString();
 }
 
+NPCItem::Type NPCItem::type() const
+{
+    return m_type;
+}
+
 void NPCItem::increaseQuantity()
 {
     m_pQuantity->setText( QString::number(m_pQuantity->text().toInt()+1) );
+}
+
+void NPCItem::decreaseQuantity()
+{
+    m_pQuantity->setText( QString::number(m_pQuantity->text().toInt()-1) );
+}
+
+void NPCItem::throwOut()
+{
+    decreaseQuantity();
+    if ( 0 == m_pQuantity->text().toInt() )
+        emit destroyItem( this );
+}
+
+void NPCItem::setType(const NPCItem::Type &type)
+{
+    m_type = type;
+    delete m_pButton->menu();
+    createActions();
+}
+
+void NPCItem::createActions()
+{
+    QMenu *pMenu = new QMenu(this);
+
+    if ( Type::SHOP == m_type ) {
+        m_pReturn = new QAction("Zwróć", this);
+        pMenu->addAction( m_pReturn );
+        connect( m_pReturn, &QAction::triggered,
+                 [this](){
+            emit this->returnItem(m_item.value("name").toString(), m_item.value("price").toInt());
+            this->decreaseQuantity();
+            if ( 0 == m_pQuantity->text().toInt() )
+                emit this->destroyItem( this );
+        });
+    }
+    else {
+        m_pThrowOut = new QAction("Wyrzuć", this);
+        pMenu->addAction( m_pThrowOut );
+        connect( m_pThrowOut, &QAction::triggered,
+                 this, &NPCItem::throwOut );
+
+        if ( "Broń biała" == m_item.value("type").toString()
+             || "Broń miotana" == m_item.value("type").toString()
+             || "Pistolet" == m_item.value("type").toString()
+             || "Rewolwer" == m_item.value("type").toString()
+             || "Pistolet maszynowy" == m_item.value("type").toString()
+             || "Karabin powtarzalny" == m_item.value("type").toString()
+             || "Karabin samopowtarzalny" == m_item.value("type").toString()
+             || "Karabin automatyczny" == m_item.value("type").toString()
+             || "Strzelba" == m_item.value("type").toString()
+             || "Karabin maszynowy" == m_item.value("type").toString()
+             || "Karabin snajperski" == m_item.value("type").toString()
+             || "Granatnik" == m_item.value("type").toString()
+             || "Broń przeciwpancerna" == m_item.value("type").toString()
+             || "Miotacz ognia" == m_item.value("type").toString()
+             || "Zbroja" == m_item.value("type").toString()
+             || "Kamizelka" == m_item.value("type").toString()
+             || "Hełm" == m_item.value("type").toString() ) {
+            m_pEquip = new QAction("Wyekwipuj", this);
+            pMenu->addAction(m_pEquip);
+            connect( m_pEquip, &QAction::triggered,
+                     [this](){
+                emit this->equip(m_item);
+                this->decreaseQuantity();
+                if ( 0 == m_pQuantity->text().toInt() )
+                    emit this->destroyItem( this );
+            });
+        }
+        else {
+            m_pUse = new QAction("Użyj", this);
+            pMenu->addAction(m_pUse);
+        }
+    }
+    m_pButton->setMenu(pMenu);
 }
--- NPC/Widgets/NPCItem.h
+++ NPC/Widgets/NPCItem.h
@@ -5,29 +5,50 @@
 
 #include <QJsonObject>
 
+class QAction;
 class QLabel;
 class QPushButton;
 
 class NPCItem : public NPCCustomWidget
 {
     Q_OBJECT
-
 public:
-    explicit NPCItem(const QJsonObject &item, QWidget *parent = nullptr);
+    enum class Type { INVENTORY, SHOP };
+
+    explicit NPCItem(const QJsonObject &item, Type type, QWidget *parent = nullptr);
 
     QString name() const;
+    Type type() const;
 
     void increaseQuantity();
+    void decreaseQuantity();
 
 signals:
+    void typeChanged(const Type &type);
     void returnItem(const QString &name, const int &price);
     void destroyItem(NPCItem *item);
+    void equip(QJsonObject &item);
+
+public slots:
+    void setType(const Type &type);
+
+private slots:
+    void throwOut();
+
+private:
+    void createActions();
 
 private:
     QJsonObject m_item;
+    Type m_type;
     QLabel *m_pName{nullptr};
     QLabel *m_pQuantity{nullptr};
-    QPushButton *m_pReturnBtn{nullptr};
+    QPushButton *m_pButton{nullptr};
+
+    QAction *m_pReturn{nullptr};
+    QAction *m_pThrowOut{nullptr};
+    QAction *m_pUse{nullptr};
+    QAction *m_pEquip{nullptr};
 };
 
 #endif // NPCITEM_H
