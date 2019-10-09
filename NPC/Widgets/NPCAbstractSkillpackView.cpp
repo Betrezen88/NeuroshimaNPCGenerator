6012fac3b9b45c90196cfd07a431d40bebe6104b
Betrezen88 <wyszyn2000@gmail.com>
2019-10-09 14:56:15 +0200

    Reworked NPCAbstractSkillpackView.

--- NPC/Widgets/NPCAbstractSkillpackView.cpp
+++ NPC/Widgets/NPCAbstractSkillpackView.cpp
@@ -1,5 +1,7 @@
 #include "NPCAbstractSkillpackView.h"
 
+#include <QJsonValue>
+#include <QJsonObject>
 #include <QGridLayout>
 #include <QLabel>
 
@@ -10,6 +12,34 @@ NPCAbstractSkillpackView::NPCAbstractSkillpackView(const QString &name, const QS
       m_pLayout( new QGridLayout ),
       m_specializations( specs )
 {
+    init();
+}
+
+NPCAbstractSkillpackView::NPCAbstractSkillpackView(const QString &name, const QJsonArray &specs, QWidget *parent)
+    : NPCCustomWidget( parent ),
+      m_pName( new QLabel(name, this) ),
+      m_pSpecs( new QLabel(this) ),
+      m_pLayout( new QGridLayout )
+{
+    for ( const QJsonValue spec: specs )
+        m_specializations << spec.toString();
+
+    init();
+}
+
+QString NPCAbstractSkillpackView::name() const
+{
+    return m_pName->text();
+}
+
+QStringList NPCAbstractSkillpackView::specializations() const
+{
+    return m_specializations;
+}
+
+void NPCAbstractSkillpackView::init()
+{
+    setSizePolicy( QSizePolicy::Expanding, QSizePolicy::Minimum );
     setLayout( m_pLayout );
     setSpecializationsLabel( m_specializations );
     m_pName->setObjectName( "Title" );
@@ -22,16 +52,6 @@ NPCAbstractSkillpackView::NPCAbstractSkillpackView(const QString &name, const QS
     pTitleRow->addWidget( m_pSpecs, 0, Qt::AlignRight );
 
     m_pLayout->addLayout( pTitleRow, 0, 0 );
-}
-
-QString NPCAbstractSkillpackView::name() const
-{
-    return m_pName->text();
-}
-
-QStringList NPCAbstractSkillpackView::specializations() const
-{
-    return m_specializations;
 }
 
 void NPCAbstractSkillpackView::setSpecializationsLabel(const QStringList &specs)
